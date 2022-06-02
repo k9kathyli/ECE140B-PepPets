c43d2530f17e2fd2afeb3d1f04e903006601f382
Kendrick <you@example.com>
2022-06-01 19:19:18 -0700

    send-receive remove

--- PepPets/Hardware/connect-friend/receive.py
+++
@@ -1,5 +0,0 @@
-import serial
-serial = serial.Serial('/dev/ttyS0', 9600)
-
-receivedData = serial.readLine()
-print(receivedData)
--- PepPets/Hardware/connect-friend/send.py
+++
@@ -1,7 +0,0 @@
-import serial
-serial = serial.Serial('/dev/ttyS0', 9600)
-
-serial.write(b'get data')
-sentData = serial.readLine()
-
-print(sentData)
