f3ee86456455d316ec91d73199eac37c4cf63629
a1chan0210 <a1chan@ucsd.edu>
2022-06-03 14:14:34 -0700

    read-write

--- PepPets/serial_read.py
+++ PepPets/serial_read.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
 import time
 import serial
 
-# Source: https://pimylifeup.com/raspberry-pi-serial/
-
-ser = serial.Serial(
-        port='/dev/ttyAM0',
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-)
+ser = serial.Serial(port='/dev/serial0', baudrate = 9600, timeout=1)
 
 while 1:
-        x=ser.readline()
-        print(x)
+    x=ser.readline()
+    print(x)
--- PepPets/serial_write.py
+++ PepPets/serial_write.py
@@ -2,19 +2,12 @@
 import time
 import serial
 
-# Source: https://pimylifeup.com/raspberry-pi-serial/
+ser = serial.Serial(port='/dev/serial0', baudrate = 9600, timeout = 1)
 
-ser = serial.Serial(
-        port='/dev/ttyAM0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-)
-counter=0
+def write():
+    ser.write(b"hello")
 
-def write_message(message):
-    while True:
-            ser.write(message)
-            time.sleep(1)
+while True:
+    write()
+    ser.flush()
+    time.sleep(1)
